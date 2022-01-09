cbf8f7b3e3e4c52fd736913019b0427156919129
Himanshu019 <94908165+Himanshu019@users.noreply.github.com>
2022-01-09 23:49:41 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,105 @@
+const path= require('path')
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode.js')
+const forecast=require('./utils/forecast.js')
+const { resolve } = require('url')
+
+const app=express()
+const port = process.env.PORT || 3000
+
+//define paths for express config
+const pubicDirectoryPath=path.join(__dirname, '../pubic')
+const viewsPath= path.join(__dirname, '../templates/views')
+const partialsPath= path.join(__dirname,'../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(path.join(__dirname,'../public')))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'Himanshu'
+    })
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name:'Himanshu'
+    })
+})
+
+app.get('/help', (req,res)=>{
+    res.render('help',{
+        title:'contact',
+        name:'89********'
+    })
+})
+
+app.get('/weather', (req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide an address.'
+        })
+    }
+    command=req.query.address
+    geocode(command, (error,data)=>{
+        if(error){
+            return res.send({
+                error
+            })
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastdata) => {
+            if(error){
+                return res.send({
+                    error
+                })
+            }
+
+            res.send({
+                location:data.location,
+                forecastdata:forecastdata
+            })    
+        })
+          
+    })   
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error:'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        para:'Help page not found',
+        name:'Himanshu'
+    })
+} )
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        para:'404 page not found',
+        name:'Himanshu'
+    })
+} )
+
+app.listen(port,()=>{
+    console.log('Server is up on port ' + port)
+})
